03065e60e9761af1b2b01f9a555e6a84af4e79b2
Abbygree <syper.alibek@gmail.com>
2020-08-06 10:53:01 +0300

    Создание структуры Task

--- Tasks.go
+++ Tasks.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
-	$END$
+import "time"
+
+type Task struct {
+	TaskID 		string 	  `json:"task_id"`
+	GroupID 	int 	  `json:"group_id"`
+	Task 		string 	  `json:"task"`
+	Completed 	bool 	  `json:"completed"`
+	CreatedAt 	time.Time `json:"created_at"`
+	CompletedAt time.Time `json:"completed_at"`
 }
